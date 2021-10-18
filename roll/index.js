5b8e586f0497242321a49456f9092428521ce282
NotMinhDucGamingTV <77107580+1stminhcar@users.noreply.github.com>
2021-10-18 16:16:08 +0700

    Update index.js

--- roll/index.js
+++ roll/index.js
@@ -38,7 +38,7 @@ const updateClassTableText = (databaseText) => {
         "|Wizard|" + getDatabaseValue(databaseText, "wizard") + "|\n";
 }
 const createClassLink = (chosenClass) => {
-    return `https://github.com/benjaminsampica/benjaminsampica/issues/new?title=roll%7C${chosenClass}&body=Just+click+%27Submit+new+issue%27.`;
+    return `https://github.com/1stminhcar/1stminhcar/issues/new?title=roll%7C${chosenClass}&body=Just+click+%27Submit+new+issue%27.`;
 }
 const updateRollTableText = (databaseText) => {
     readMeText += "|Roll|Count|\n" +
@@ -74,19 +74,92 @@ rollDatabaseText = setDatabaseValue(rollDatabaseText, `-${roll}-`, newRollValue)
 
 const user = core.getInput('user');
 let readMeText =
-    "# ROLL FOR INITIATIVE\n" +
-    "### CHOOSE YOUR CLASS\n" +
+    # Hi there, I'm NotMinhDucGamingTV <img width="30px" src="https://github.com/SatYu26/SatYu26/raw/master/Assets/Hi.gif" />"\
+
+<img align="right" alt="GIF" height="160px" src="https://octodex.github.com/images/daftpunktocat-guy.gif" />
+
+## I'm learning to be a Discord Bot Developer
+
+- 🌱 I am new in Discord.JS:)
+- 🏆 2021 Goals: Contribute to more open source projects and upgrading my [discord bot!](https://discord.com/api/oauth2/authorize?client_id=892340813906968587&permissions=0&redirect_uri=https%3A%2F%2Fwebsite-for-my-bot.herokuapp.com%2Fapi%2Fv1%2Fauth&scope=bot%20applications.commands)
+- 📫 What is the best way to contact me? [Discord](https://discord.com/users/470047132670361610)
+- 😄 Pronouns: He/Him
+
+[![GitHub](https://img.shields.io/badge/Github-100000?style=for-the-badge&logo=github&logoColor=white)](https://github.com/1stminhcar)
+[![Discord](https://img.shields.io/badge/Discord-7289DA?style=for-the-badge&logo=discord&logoColor=white)](https://discord.gg/YpbvvR9SX8)
+[![Reddit](https://img.shields.io/badge/Reddit-FF4500?style=for-the-badge&logo=reddit&logoColor=white)](https://www.reddit.com/user/Unlikely-Agent)
+
+---
+
+<img align="right" alt="GIF" height="170px" src="https://media.giphy.com/media/J5B1Y8QZnzXXbLQIBu/giphy.gif" />
+  Listening On Spotify:
+
+[![Spotify](https://novatorem-kyzbk7wxl-bardiesel.vercel.app/api/spotify)](https://open.spotify.com/user/31xhadxgkhc6hcgu6qvu5snptxh4)
+
+---
+<!--START_SECTION:waka-->
+**My Commits:** 
+
+```text
+🌞 Morning    1 commits    ████████░░░░░░░░░░░░░░░░░  25%
+🌆 Daytime    1 commits    ████████░░░░░░░░░░░░░░░░░  25%
+🌃 Evening    1 commits    ████████░░░░░░░░░░░░░░░░░  25%
+🌙 Night      1 commits    ████████░░░░░░░░░░░░░░░░░  25%
+```
+<!--END_SECTION:waka-->
+
+---
+
+### OS and Cloud Services
+[![Android](https://img.shields.io/badge/Android-3DDC84?style=for-the-badge&logo=android&logoColor=white)](https://android.com)
+[![Windows](https://img.shields.io/badge/Windows-0078D6?style=for-the-badge&logo=windows&logoColor=white)](https://microsoft.com/windows)
+[![Heroku](https://img.shields.io/badge/Heroku-430098?style=for-the-badge&logo=heroku&logoColor=white)](https://heroku.com)
+[![Repl.it](https://img.shields.io/badge/replit-667881?style=for-the-badge&logo=replit&logoColor=white)](https://replit.com)
+[![Glitch](https://img.shields.io/badge/Glitch-2800ff?style=for-the-badge&logo=glitch&logoColor=white)](https://glitch.com)
+
+### Browsers and IDE
+[![Google Chrome](https://img.shields.io/badge/Google_chrome-4285F4?style=for-the-badge&logo=Google-chrome&logoColor=white)](https://google.com/chrome/)
+[![GitHub](https://img.shields.io/badge/Github-100000?style=for-the-badge&logo=github&logoColor=white)](https://github.com)
+[![Notepad++](https://img.shields.io/badge/Notepad++-90E59A.svg?style=for-the-badge&logo=notepad%2B%2B&logoColor=black)](https://notepad-plus-plus.org)
+
+### Github Stats
+[![trophy](https://github-profile-trophy.vercel.app/?username=1stminhcar&theme=onedark&title=Joined2020,Commit,Followers,Repositories,Stars,PullRequest)](https://github.com/ryo-ma/github-profile-trophy)
+![Simpleboy353's GitHub stats](https://github-readme-stats.vercel.app/api?username=1stminhcar&show_icons=true&theme=radical)
+[![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=1stminhcar)](https://github.com/anuraghazra/github-readme-stats)
+
+---
+## Statistic
+![Metrics](https://metrics.lecoq.io/1stminhcar?template=classic&config.timezone=Asia%2FHo_Chi_Minh)
+---
+Recent Commmit:
+  <!--START_SECTION:activity-->
+1. ❗️ Opened issue [#11](https://github.com/1stminhcar/1stminhcar/issues/11) in [1stminhcar/1stminhcar](https://github.com/1stminhcar/1stminhcar)
+2. 🎉 Merged PR [#10](https://github.com/1stminhcar/1stminhcar/pull/10) in [1stminhcar/1stminhcar](https://github.com/1stminhcar/1stminhcar)
+3. 💪 Opened PR [#10](https://github.com/1stminhcar/1stminhcar/pull/10) in [1stminhcar/1stminhcar](https://github.com/1stminhcar/1stminhcar)
+4. ❗️ Opened issue [#17356](https://github.com/timburgan/timburgan/issues/17356) in [timburgan/timburgan](https://github.com/timburgan/timburgan)
+5. ❗️ Opened issue [#9](https://github.com/1stminhcar/1stminhcar/issues/9) in [1stminhcar/1stminhcar](https://github.com/1stminhcar/1stminhcar)
+5.
+<!--END_SECTION:activity-->
+
+##Visitor:![visitors](https://visitor-badge.glitch.me/badge?page_id=1stminhcar.1stminhcar&left_color=black&right_color=black)
+
+---
+## Minigame Corner:
+# ROLL FOR INITIATIVE
+"### CHOOSE YOUR CLASS\n" +
     `\n[Warrior | +1 To Roll](${createClassLink("warrior")})\n` +
     `\n[Cleric | +1 To Roll](${createClassLink("cleric")})\n` +
     `\n[Rogue | +3 To Roll](${createClassLink("rogue")})\n` +
     `\n[Wizard | -1 To Roll](${createClassLink("wizard")})\n` +
     "### LAST ROLL BY\n" +
     `[${user}](https://www.github.com/${user}) - as a ${usersClass} rolled a ${roll}!` +
-    "\n\n";
+    "\n\n";"\
 
 updateClassTableText(classDatabaseText);
 readMeText += "\n";
 updateRollTableText(rollDatabaseText);
+<img src="https://imgur.com/rilHVxA.png"/> 
+
 
 fs.writeFileSync('./README.md', readMeText);
 fs.writeFileSync('./rollDatabase.txt', rollDatabaseText);
